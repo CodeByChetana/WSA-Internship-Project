6c0c4d9e52ecd8ef2fdc6519b71252117b65d7a3
Chetana <kchetana2149@gmail.com>
2026-09-19 01:27:54 +0530

    Updated server with database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+import connectDB from "./utils/db.js";
 dotenv.config();
 
 const app = express();
+//express.json
+app.use(express.json({limit:"100mb"}))
+
+//urlencoded
+app.use(express.urlencoded({limit:"100mb",extended:true}))
+
+//cookieParser
+app.use(cookieParser())
+
 
 const PORT=process.env.PORT;
 
@@ -11,6 +23,8 @@ const PORT=process.env.PORT;
 app.get("/",(req,res)=>{
     res.send("HomelyHub srever is running")
 })
+
+connectDB();
 
 app.listen(PORT,()=>{
     console.log(`App is running on port no1 ${PORT}`);
